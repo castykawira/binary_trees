7ba1c08d63b0ee28f05b101e0fe0a5bd931f917e
castykawira <murithicasty@gmail.com>
2023-11-01 10:32:31 -0700

    question 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	lh = binary_tree_height(tree->left);
 	rh = binary_tree_height(tree->right);
 
