357e62380d767c091ccfbd1c43aaf3b036d4753e
Bimacobiz <collinsmahigi@gmail.com>
2023-11-02 15:47:37 -0700

    changes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,37 +10,37 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	int depth1 = 0, depth2 = 0;
-    binary_tree_t *node1 = (binary_tree_t *)first;
-    binary_tree_t *node2 = (binary_tree_t *)second;
+	binary_tree_t *node1 = (binary_tree_t *)first;
+	binary_tree_t *node2 = (binary_tree_t *)second;
 
 
-    while (node1) {
-        depth1++;
-        node1 = node1->parent;
-    }
+	while (node1) {
+		depth1++;
+		node1 = node1->parent;
+	}
 
-    while (node2) {
-        depth2++;
-        node2 = node2->parent;
-    }
+	while (node2) {
+		depth2++;
+		node2 = node2->parent;
+	}
 
-    node1 = (binary_tree_t *)first;
-    node2 = (binary_tree_t *)second;
+	node1 = (binary_tree_t *)first;
+	node2 = (binary_tree_t *)second;
 
-    while (depth1 > depth2) {
-        node1 = node1->parent;
-        depth1--;
-    }
+	while (depth1 > depth2) {
+		node1 = node1->parent;
+		depth1--;
+	}
 
-    while (depth2 > depth1) {
-        node2 = node2->parent;
-        depth2--;
-    }
+	while (depth2 > depth1) {
+		node2 = node2->parent;
+		depth2--;
+	}
 
-    while (node1 != node2) {
-        node1 = node1->parent;
-        node2 = node2->parent;
-    }
+	while (node1 != node2) {
+		node1 = node1->parent;
+		node2 = node2->parent;
+	}
 
-    return node1;
+	return node1;
 }
