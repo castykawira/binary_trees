f4b52f18e851c69df38952567795065fbd645805
castykawira <murithicasty@gmail.com>
2023-11-02 05:29:14 -0700

    question 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,59 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Determines if a binary tree is perfect
- * @tree: A pointer to the root node of the binary tree
- * Return: 1 if the binary tree is perfect, or 0 otherwise or if tree is NULL
+ * binary_tree_size - computes the size of a binary tree
+ * @tree: A Pointer to the root node of the binary tree
+ * Return: The size of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_is_perfect - Dtermines if a binary tree is perfect
+ * @tree: A Pointer to the root node of the binary tree
+ * Return: 1 if the perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int lh;
-	int rh;
+	size_t height, size;
 
 	if (tree == NULL)
 		return (0);
 
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
 
-	return (binary_tree_is_full(tree) && lh == rh);
-}
-
-/**
- * binary_tree_is_full - Determines if a binary tree is full
- * @tree: A pointer to the root node of the binary tree
- * Return: 1 if the binary tree is full, or 0 otherwise or if tree is NULL
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
-
-	return (0);
-}
-
-/**
- * binary_tree_height - Computes the height of a binary tree
- * @tree: A pointer to the root node of the binary tree
- * Return: The height of the binary tree, or 0 if tree is NULL
- */
-int binary_tree_height(const binary_tree_t *tree)
-{
-	int lh;
-	int rh;
-
-	if (tree == NULL)
-		return (0);
-
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-
-	return (1 + (lh > rh ? lh : rh));
+	return ((1 << height) - 1 == size);
 }
