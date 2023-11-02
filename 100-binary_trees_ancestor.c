b56ce5cfbde4ffd4e06cd5288817e4dc74326047
Bimacobiz <collinsmahigi@gmail.com>
2023-11-02 16:00:35 -0700

    changes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,11 +2,11 @@
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: points to the initial node
- * @second: points to the second node
+ * @second: points to theecond node
  *
  * Return: lowest common ancestor or NULL if common ancestor is absent
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	int depth1 = 0, depth2 = 0;
@@ -14,12 +14,14 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	binary_tree_t *node2 = (binary_tree_t *)second;
 
 
-	while (node1) {
+	while (node1)
+	{
 		depth1++;
 		node1 = node1->parent;
 	}
 
-	while (node2) {
+	while (node2)
+	{
 		depth2++;
 		node2 = node2->parent;
 	}
@@ -27,20 +29,23 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	node1 = (binary_tree_t *)first;
 	node2 = (binary_tree_t *)second;
 
-	while (depth1 > depth2) {
+	while (depth1 > depth2)
+	{
 		node1 = node1->parent;
 		depth1--;
 	}
 
-	while (depth2 > depth1) {
+	while (depth2 > depth1)
+	{
 		node2 = node2->parent;
 		depth2--;
 	}
 
-	while (node1 != node2) {
+	while (node1 != node2)
+	{
 		node1 = node1->parent;
 		node2 = node2->parent;
 	}
 
-	return node1;
+	return (node1);
 }
