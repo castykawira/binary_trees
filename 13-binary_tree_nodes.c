dc60a6d7f51f1563f4b6d40ac699f9cc112fa7bd
Bimacobiz <collinsmahigi@gmail.com>
2023-11-01 19:38:46 -0700

    changes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - counts nodes with at least i child in a given binary tree
+ * binary_tree_nodes - counts nodes with at least i child in given binary tree
  * @tree: points to the root node of the tree whose nodes should be counted
  * Return: 0 if the tree is NULL
  */
@@ -13,4 +13,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	else
 		return (0);
-}	
+}
