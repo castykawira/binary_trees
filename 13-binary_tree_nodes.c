91258d43a9f71b938f527d805f72fda75a757934
Bimacobiz <collinsmahigi@gmail.com>
2023-11-01 19:12:03 -0700

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,14 +5,12 @@
  * Return: 0 if the tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
-{	
-	size_t numbered_nod = 0;
+{
+	if (tree == NULL)
+		return (0);
 
-	if (tree)
-	{
-		numbered_nod += (tree->left || tree->right) ? 1 : 0;
-		numbered_nod += binary_tree_nodes(tree->left);
-		numbered_nod += binary_tree_nodes(tree->right);
-	}
-	return (numbered_nod);
-}
+	if (tree->left || tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+		return (0);
+}	
